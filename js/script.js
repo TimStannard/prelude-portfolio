eded76bfb0f3b17be4b7e83b0020886c5ae9feb5
Josh van der Waay <270025670@bun2-206-02.acg.ac.nz>
2021-06-29 16:29:05 +1200

    worked on slideshow

--- js/script.js
+++ js/script.js
@@ -693,6 +693,62 @@ $(document).ready(function () {
              '</a>'+
            '</div>'+
 
+        '<!-- Container for the image gallery -->'+
+        '<div class="container">' +
+        '<!-- Full-width images with number text -->' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">1 / 6</div>' +
+            '<img src="img_woods_wide.jpg" style="width:100%">' +
+       '</div>' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">2 / 6</div>' +
+            '<img src="img_5terre_wide.jpg" style="width:100%">' +
+        '</div>' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">3 / 6</div>' +
+            '<img src="img_mountains_wide.jpg" style="width:100%">' +
+        '</div>' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">4 / 6</div>' +
+            '<img src="img_lights_wide.jpg" style="width:100%">' +
+        '</div>' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">5 / 6</div>' +
+            '<img src="img_nature_wide.jpg" style="width:100%">' +
+        '</div>' +
+        '<div class="mySlides">' +
+            '<div class="numbertext">6 / 6</div>' +
+            '<img src="img_snow_wide.jpg" style="width:100%">' +
+        '</div>' +
+        '<!-- Next and previous buttons -->' +
+        '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>' +
+        '<a class="next" onclick="plusSlides(1)">&#10095;</a>' +
+        '<!-- Image text -->' +
+        '<div class="caption-container">' +
+            '<p id="caption"></p>' +
+        '</div>' +
+        '<!-- Thumbnail images -->' +
+        '<div class="row">' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">' +
+            '</div>' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">' +
+            '</div>' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">' +
+            '</div>' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">' +
+            '</div>' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">' +
+            '</div>' +
+            '<div class="column">' +
+            '<img class="demo cursor" src="img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">' +
+            '</div>' +
+        '</div>' +
+        '</div>' +
 
                 '<div class="container">'+
                 '<div class="row">'+
@@ -706,29 +762,20 @@ $(document).ready(function () {
                         '<h4>Contact Links</h4>'+
                         '<ul>'+
                           '<li>'+ studentsWUX[j].social +'</li>'+
-                          '<li>Lorem Ipsum</li>'+
-                          '<li>Lorem Ipsum</li>'+
-                          '<li>Lorem Ipsum</li>'+
                         '</ul>'+
                     '</div>'+
                   '</div>'+
                 '</div>'+
               '</div>'+
-            //   '<div class="modal-footer">'+
-            //     '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>'+
-            //     '<button type="button" class="btn btn-primary">Understood</button>'+
-            //   '</div>'+
             '</div>'+
           '</div>'+
-          '</div>'
-
-
+        '</div>'
         ); //end of apprend
     }
 
     $('.student-link').click(function(){
         event.preventDefault();
-    })// preventing auto refersh
+    })// preventing auto refresh
 
     // ==========================================================
     // Function to display all Creative Digital Design students
@@ -773,7 +820,44 @@ $(document).ready(function () {
   
 }); //Document Ready Jquery
 
+// ==========================================================
+// W3 IMG SLIDESHOW
+// ==========================================================
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
 
 
